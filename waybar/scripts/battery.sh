a5b3fbd5a3709efc1243ba19a0e6f8a8add792ee
Parshaw <parshawbhattacharjee@gmail.com>
2026-01-04 22:01:48 +0530

    test initial

--- waybar/scripts/battery.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if ls /sys/class/power_supply/BAT* &>/dev/null; then
-  ln -sf ~/.config/waybar/style-laptop.css ~/.config/waybar/style.css
-else
-  ln -sf ~/.config/waybar/style-desktop.css ~/.config/waybar/style.css
-fi
-pkill -SIGUSR2 waybar
